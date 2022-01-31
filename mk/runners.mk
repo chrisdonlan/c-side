4ea04928d02337f88859e3fcfe201275475f2010
Christopher Donlan <cdonlan@evidentid.com>
2022-01-31 16:01:52 -0500

    increased the opportunity for the api to free the pix memory by also freeing the associated image data prior to calling the destructor.

--- mk/runners.mk
+++ mk/runners.mk
@@ -1,2 +1,2 @@
 run:=./
-valgrind-memory:=valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt 
+valgrind-memory:=valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt # leave trailing space
--- ocr-memtest-c.c
+++ ocr-memtest-c.c
@@ -33,20 +33,30 @@ int main(int argc, char **argv){
         // Pix seems to delete the data associated with the original pix when it is set to a different pix
         // so this avoids segfault
         struct Pix * r_pix_cp = pixCopy(NULL, r_pix);
+
         l_uint32 * pix_data = pixExtractData(r_pix_cp);
 
         // analog for python3 conversion from PIL
         struct Pix * pix = pixCreate(r_pix->w, r_pix->h, r_pix->d);
         pixSetData(pix, pix_data);
         struct Pix * pix2 = pixEndianByteSwapNew(pix);
+
+        // free the data
+        pixFreeData(pix2);
+        pixFreeData(pix);
+        pixFreeData(r_pix_cp);
+
+        // destroy the structs
         pixDestroy(&pix2);
         pixDestroy(&pix);
-
-        // free the rest of the cp struct
         pixDestroy(&r_pix_cp);
+        free(pix_data);
+
+        // status update
         printf("CYCLE: {%d|%d} complete\n", i + 1, I);
     };
 
     // free the original pix
+    pixFreeData(r_pix);
     pixDestroy(&r_pix);
 }
