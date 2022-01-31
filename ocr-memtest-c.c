3185b78e12d1df178f58f5efca4b064274d01343
Chris Donlan <christopher.j.donlan@gmail.com>
2022-01-31 15:02:11 -0500

    cleaned up test prior to valgrind.

--- ocr-memtest-c.c
+++ ocr-memtest-c.c
@@ -4,23 +4,24 @@
 
 int main(int argc, char **argv){
 
-		if (argc < 2){
-			printf("Usage: <this> <image-file>\n");
-			return 1;
-		}
+    if (argc < 2){
+      printf("Usage: <this> <image-file>\n");
+      return 1;
+    }
 
-		char * image = argv[1];
+    char * image = argv[1];
 
-		if (strlen(image) == 0){
-			printf("foo Usage: <this> <image-file>\n");
-			return 1;
-		} else {
-			printf("image: %s\n",argv[1]);
-		}
+    if (strlen(image) == 0){
+      printf("foo Usage: <this> <image-file>\n");
+      return 1;
+    } else {
+      printf("image: %s\n",image);
+    }
 
+    printf("Reading image %s\n", image);
     struct Pix * r_pix = pixRead(image);
 
-		int I = 100;
+    int I = 100;
     for (int i = 0; i < I; i++){
 
         // Pix seems to delete the data associated with the original pix when it is set to a different pix
@@ -36,7 +37,7 @@ int main(int argc, char **argv){
 
         // free the rest of the cp struct
         pixDestroy(&r_pix_cp);
-				printf("%d of %d cycles completed.", i, I);
+        printf("CYCLE: {%d|%d} complete\n", i + 1, I);
     };
 
     // free the original pix
